b2154c0d098d3f68bced2f4db71bb9d250ee6db7
EmilioMendozaCortes <emiliomendocor@gmail.com>
2024-11-05 16:21:12 -0600

    Corrección de la estructura de vue3

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,43 @@
 <template>
   <v-app>
+    <v-toolbar app>
+      <v-icon>arrow_back</v-icon>
+      <v-toolbar-title>
+        <span>VueNoteApp</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-content>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWord msg="Bienvenido a tu app de Vue.js"/>
+    </v-content>
+  </v-app>
+</template>
+
+
+    <!-- <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          containt
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        ></v-img>
+      </div>
+    </v-app-bar>
+    
     <v-main>
       <HelloWorld/>
     </v-main>
   </v-app>
-</template>
+</template>-->
 
-<script>
+<!-- <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -20,4 +51,4 @@ export default {
     //
   }),
 }
-</script>
+</script>  -->
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
+// import Vue from 'vue'
+// import Vuetify from 'vuetify'
+import vuetify from './plugins/vuetify'
+
+import 'vuetify/dist/vuetify.min.css'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
@@ -8,3 +14,20 @@ loadFonts()
 createApp(App)
   .use(vuetify)
   .mount('#app')
+
+// import Vue from 'vue'
+// import App from './App.vue'
+// import Vuetify from 'vuetify'
+// import vuetify from './plugins/vuetify'
+
+// import 'vuetify/dist/vuetify.min.css'
+// import 'roboto-fontface/css/roboto/roboto-fontface.css'
+// import '@mdi/font/css/materialdesignicons.css'
+
+// Vue.config.productionTip = false
+// Vue.use(vuetify)
+
+// new Vue({
+//   vuetify,
+//   render: h => h(App)
+// }).$mount('#app')
